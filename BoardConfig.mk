e4b873bdacf999d5667544c54154277ab772f409
Guillaume La Roque <glaroque@baylibre.com>
2019-04-29 15:42:05 +0000

    Update BoardConfig.mk
    Change-Id: I1705adee3e53d56606b0ecd8bc01c05f314da915
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 #
 # Product-specific compile-time definitions.
 #
-
 # The generic product target doesn't have any hardware-specific pieces.
 # Primary Arch
 TARGET_ARCH := arm64
@@ -17,15 +16,24 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_USES_64_BIT_BINDER := true
+TARGET_SUPPORTS_64_BIT_APPS := true
 
-USE_OPENGL_RENDERER := true
+TARGET_BOARD_PLATFORM := yukawa
 
 # BT configs
-BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH := false
+
+# disable Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := false
+PRODUCT_FULL_TREBLE := false
+PRODUCT_TREBLE_LINKER_NAMESPACES := false
+# BOARD_VNDK_VERSION := current
 
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := true
 
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 268435456
@@ -41,5 +49,26 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
+BOARD_KERNEL_OFFSET      := 0x1080000
+BOARD_KERNEL_TAGS_OFFSET := 0x1000000
+BOARD_MKBOOTIMG_ARGS     := --second $(sort $(wildcard device/amlogic/yukawa-kernel/*.dtb)) --kernel_offset $(BOARD_KERNEL_OFFSET) --second_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+BOARD_KERNEL_CMDLINE += root=PARTUUID=99f9b7ac-5207-11e9-8507-c3c037e393f3
+BOARD_KERNEL_CMDLINE += no_console_suspend console=ttyAML0,115200 earlycon
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive printk.devkmsg=on
+BOARD_KERNEL_CMDLINE += skip_initramfs init=/init
+
+
+USE_E2FSPROGS := true
+
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_MKE2FS := true
+TARGET_USES_HWC2 := true
+
 BOARD_SEPOLICY_DIRS += \
         device/amlogic/yukawa/sepolicy
+
+DEVICE_MANIFEST_FILE := device/amlogic/yukawa/manifest.xml
+DEVICE_MATRIX_FILE := device/amlogic/yukawa/compatibility_matrix.xml
+
