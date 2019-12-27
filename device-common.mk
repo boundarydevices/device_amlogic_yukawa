2a140edb5f44db4be044fa6e42151bf4ed41d4a3
Tomasz Wasilczyk <twasilczyk@google.com>
2019-12-27 09:28:10 -0800

    Remove Broadcast Radio HAL 1.x implementation.
    Bug: 146456245
    Test: none
    Change-Id: I566b14aaef006edba753ef026e65f3413656e47b

--- device-common.mk
+++ device-common.mk
@@ -106,7 +106,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.1-impl \
 
 # Hardware Composer HAL
