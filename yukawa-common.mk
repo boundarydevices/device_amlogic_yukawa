b81b8f61070906858b4cf7f9bb56c878828017e5
Dmitry Shmidt <dimitrysh@google.com>
2020-01-30 10:21:42 -0800

    yukawa: Add PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
    Bug: 148602753
    Test: make -j24
    
    Change-Id: Iade61f1433132c43cd3e123aeb0cf103c942b5cd
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- yukawa-common.mk
+++ yukawa-common.mk
@@ -5,6 +5,7 @@ $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_SUPPORTS_BOOT_SIGNER := false
 
 PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := ATV on yukawa
