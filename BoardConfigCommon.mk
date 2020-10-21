boundarydevices/device_amlogic_yukawa

commit 6a6d32aa374f440e2397bfe09956f0c8a51257b2

yukawa: rework folder tree
- move all hal in hal subfolder
- move all runtime bins in binaries subfolder
- rework bootctrl manifest to be included with hal

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>
Change-Id: I61f7d60f362a5781070694488df923663da315f7

hal/bootctrl/Android.bp
@@ -17,6 +17,7 @@ cc_library_shared {
     vendor: true,
     recovery_available: true,
     relative_install_path: "hw",
+    vintf_fragments: ["bootctrl.xml"],
 
     srcs: [
         "boot_control.cc",

commit 144266823c290fac7ebeaf81ee41e8f044d1af67

yukawa: boot: don't use vintf_fragments for boot hal
With commit 6a6d32aa374f ("yukawa: rework folder tree"), we have added
the boot control hal as a vintf fragment instead of a separate manifest.

Because we also use 'recovery_available: true", the bootctrl.xml is
installed in 2 places:
- system/etc/vintf/manifest/bootctrl.xml (for recovery)
- vendor/etc/vintf/manifest/bootctrl.xml (for normal Android)

The above breaks checkvintf as following:

> getFrameworkHalManifest: -2147483648 VINTF parse error: Cannot add
> manifest fragment /system/etc/vintf/manifest/bootctrl.xml: Cannot add a
> device manifest to a framework manifest

Fix it by removing the usage of vintf_fragments.
Also, update the filepath for the device fragment to point towards the
correct file.

Test: make TARGET_VIML3=true TARGET_KERNEL_USE=4.19 TARGET_USE_AB_SLOT=true
Fixes: 6a6d32aa374f ("yukawa: rework folder tree")
Change-Id: Ifb3ab5a8c7191f8bbb8a502d289a6521f872ff10
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

BoardConfigCommon.mk
@@ -145,7 +145,7 @@ BOARD_SEPOLICY_DIRS += \
 
 DEVICE_MANIFEST_FILE := device/amlogic/yukawa/manifest.xml
 ifeq ($(TARGET_USE_AB_SLOT), true)
-DEVICE_MANIFEST_FILE += device/amlogic/yukawa/manifest_ab.xml
+DEVICE_MANIFEST_FILE += device/amlogic/yukawa/hal/bootctrl/bootctrl.xml
 endif
 
 ifeq ($(TARGET_KERNEL_USE), 5.4)

hal/bootctrl/Android.bp
@@ -17,7 +17,6 @@ cc_library_shared {
     vendor: true,
     recovery_available: true,
     relative_install_path: "hw",
-    vintf_fragments: ["bootctrl.xml"],
 
     srcs: [
         "boot_control.cc",