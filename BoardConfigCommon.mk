3585c7288eec9be1b56e25cbc2422f8c1b19d4a2
Dmitry Shmidt <dimitrysh@google.com>
2020-12-04 16:18:07 -0800

    yukawa: Accommodate any 5.x kernel
    Bug: 173215345
    Test: Manual
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    Change-Id: I7fe88733140a736566348b590ec3fc9eb6c9c1ad

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,7 +151,7 @@ ifeq ($(TARGET_USE_AB_SLOT), true)
 DEVICE_MANIFEST_FILE += device/amlogic/yukawa/hal/bootctrl/bootctrl.xml
 endif
 
-ifeq ($(TARGET_KERNEL_USE), 5.4)
+ifneq ($(TARGET_KERNEL_USE), 4.19)
 DEVICE_MANIFEST_FILE += device/amlogic/yukawa/manifest_kernel5.xml
 endif
 DEVICE_MATRIX_FILE := device/amlogic/yukawa/compatibility_matrix.xml
